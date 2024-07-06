2f367076127be5aa11f9b95fe38908fb80e6d673
Sangramjit150 <sangramjitbaksi@gmail.com>
2024-07-06 07:53:58 +0000

    [LeetCode Sync] Runtime - 1 ms (98.96%), Memory - 42.3 MB (100.00%)

---
+++ my-folder/3421-count-pairs-that-form-a-complete-day-i/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+  
+    public int countCompleteDayPairs(int[] hours) {
+        int count=0;
+        for(int i=0;i<hours.length;i++){
+          for(int j=i+1;j<hours.length;j++){
+            int sum=hours[i]+hours[j];
+            if((sum%24)==0)
+            count++;
+          }
+        }
+        return count;
+    }
+}
