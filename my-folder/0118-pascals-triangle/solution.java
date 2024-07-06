465d91d2ac98b194089612bfa8aaa4e9a504a3b8
Sangramjit150 <sangramjitbaksi@gmail.com>
2024-07-06 07:40:25 +0000

    [LeetCode Sync] Runtime - 1 ms (93.94%), Memory - 41.5 MB (99.99%)

---
+++ my-folder/0118-pascals-triangle/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+  public static int calculatePascal(int n,int r){
+        int res=1;
+        for(int i=0;i<r;i++){
+            res=res*(n-i);
+            res=res/(i+1);
+        }
+        return res;
+    }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>>ans=new ArrayList<>();
+        for(int row=1;row<=numRows;row++){
+          List<Integer>temp=new ArrayList<>();
+          for(int col=1;col<=row;col++){
+            temp.add(calculatePascal(row-1,col-1));
+          }
+          ans.add(temp);
+        }
+        return ans;
+    }
+}
