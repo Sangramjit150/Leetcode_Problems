5b705787162c95da4c47085835b988b29fe15473
Sangramjit150 <sangramjitbaksi@gmail.com>
2025-07-25 11:44:41 +0000

    [LeetCode Sync] Runtime - 2 ms (79.20%), Memory - 47.7 MB (99.72%)

---
+++ my-folder/0064-minimum-path-sum/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minsum(int i,int j,int grid[][],int dp[][]){
+        if(i==0 && j==0)
+        return grid[0][0];
+        if(i<0 || j<0)
+        return 99999;
+
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int up=minsum(i-1,j,grid,dp);
+        int left=minsum(i,j-1,grid,dp);
+        return dp[i][j]=grid[i][j]+Math.min(up,left);
+    }
+    public int minPathSum(int[][] grid) {
+       int m=grid.length;
+       int n=grid[0].length;
+       int dp[][]=new int [m][n];
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            dp[i][j]=-1;
+        }
+       }
+       return minsum(m-1,n-1,grid,dp);
+        
+    }
+}
