16ca61c56ce6e051907691ee66bf35e42be6706c
Sangramjit150 <sangramjitbaksi@gmail.com>
2025-09-22 15:07:30 +0000

    [LeetCode Sync] Runtime - 5 ms (61.94%), Memory - 42.7 MB (99.98%)

--- my-folder/0242-valid-anagram/solution.java
+++ my-folder/0242-valid-anagram/solution.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-       if(s.length()!=t.length())
-       return false; 
-       int freq[]=new int [26];
-       for(int i=0;i<s.length();i++){
-        freq[s.charAt(i)-'a']++;
-
-       }
-       for(int i=0;i<t.length();i++){
-        freq[t.charAt(i)-'a']--;
-       }
-       for(int i=0;i<26;i++){
-        if(freq[i]!=0){
+        if(s.length()!=t.length())
+        return false;
+        int count[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]!=0)
             return false;
         }
-       }
-       return true;
+        return true;
     }
-
 }
