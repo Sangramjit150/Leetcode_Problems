0179bcd43992ef35360189ec167be30e42c88319
Sangramjit150 <sangramjitbaksi@gmail.com>
2025-09-26 07:23:08 +0000

    [LeetCode Sync] Runtime - 3 ms (93.17%), Memory - 60 MB (100.00%)

---
+++ my-folder/2591-frog-jump-ii/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxJump(int[] stones) {
+        int res=stones[1]-stones[0];
+        for(int i=2;i<stones.length;i=i+2){
+            res=Math.max(res,stones[i]-stones[i-2]);
+        }
+        for(int i=3;i<stones.length;i=i+2){
+            res=Math.max(res,stones[i]-stones[i-2]);
+        }
+
+        return res;
+    }
+}
