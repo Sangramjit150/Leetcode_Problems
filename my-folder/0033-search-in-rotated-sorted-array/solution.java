211d357094a9bfce590bdf04ef4a5a657344d90a
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-05-12 05:11:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44 MB (27.10%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.java
+++ my-folder/0033-search-in-rotated-sorted-array/solution.java
@@ -1,28 +1,27 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int low=0;int high=nums.length-1;
+    public int search(int[] arr, int target) {
+        int low=0,high=arr.length-1;
         while(low<=high){
-          int mid=(low+high)/2;
-          if(nums[mid]==target)
-          return mid;
-          if(nums[low]<=nums[mid]){
-            //left half is sorted
-            if(nums[low]<=target && target<=nums[mid]){
-              high=mid-1;
+            int mid=(low+high)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=target && target<=arr[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
             }
             else{
-              low=mid+1;
+                if(arr[mid]<=target && target<=arr[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
             }
-
-          }
-          else{
-            if(nums[mid]<=target && target<=nums[high]){
-              low=mid+1;
-            }
-            else{
-              high=mid-1;
-            }
-          }
         }
         return -1;
     }
