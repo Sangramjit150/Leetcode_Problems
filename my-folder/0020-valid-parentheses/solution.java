81f1cc995389abbedcb72b400ae44664ce61395e
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-05-18 16:27:40 +0000

    [LeetCode Sync] Runtime - 2 ms (98.05%), Memory - 43.3 MB (56.90%)

--- my-folder/0020-valid-parentheses/solution.java
+++ my-folder/0020-valid-parentheses/solution.java
@@ -1,17 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-       Stack<Character>st=new Stack<>();
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)=='('){
-                st.push(')');
-            }else if(s.charAt(i)=='{'){
-                st.push('}');
-            }else if(s.charAt(i)=='['){
-                st.push(']');
-            }else if(st.isEmpty() || st.pop()!=s.charAt(i)){
-                return false;
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
             }
         }
-        return st.isEmpty();
+        return stack.isEmpty();
     }
 }
