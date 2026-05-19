7ea67c2aa8759242803251b5d9e85cfb602b4e96
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-05-19 07:40:18 +0000

    [LeetCode Sync] Runtime - 2 ms (88.77%), Memory - 48.2 MB (8.97%)

--- my-folder/0055-jump-game/solution.java
+++ my-folder/0055-jump-game/solution.java
@@ -1,14 +1,12 @@
-class Solution{
-
-
+class Solution {
     public boolean canJump(int[] nums) {
-       int reachable = 0;
-       for(int i = 0; i < nums.length; i ++) {
-           if(i > reachable) return false;
-           reachable = Math.max(reachable, i + nums[i]);
-       } 
-       return true;
+        int currInd=-1;
+        int maxInd=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>maxInd)
+            return false;
+            maxInd=Math.max(maxInd,i+nums[i]);
+        }
+        return true;
     }
 }
-    
-
