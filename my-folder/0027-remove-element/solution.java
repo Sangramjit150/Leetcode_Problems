24c2dd0530a74e6134dd61e1cec1d9d1ab48623d
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-05-19 02:35:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.5 MB (42.01%)

--- my-folder/0027-remove-element/solution.java
+++ my-folder/0027-remove-element/solution.java
@@ -3,7 +3,9 @@ class Solution {
         int cnt=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=val){
-                nums[cnt++]=nums[i];
+                nums[cnt]=nums[i];
+                cnt++;
+
             }
         }
         return cnt;
