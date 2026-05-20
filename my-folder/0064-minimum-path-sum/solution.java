c8560317286bd6d9e0aba979a43a8e1063f8d255
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-05-20 14:03:04 +0000

    [LeetCode Sync] Runtime - 3 ms (74.46%), Memory - 50.5 MB (10.12%)

--- my-folder/0064-minimum-path-sum/solution.java
+++ my-folder/0064-minimum-path-sum/solution.java
@@ -1,28 +1,18 @@
 class Solution {
-
-    private int minSum(int grid[][],int i,int j,int dp[][]){
-        if(i==0 && j==0)
-        return grid[0][0];
-        if(i<0 || j<0)
-        return 99999;
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        int left=grid[i][j]+minSum(grid,i,j-1,dp);
-        int up=grid[i][j]+minSum(grid,i-1,j,dp);
-        int ans=Math.min(up,left);
-        dp[i][j]=ans;
-        return dp[i][j];
-    }
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        int dp[][]=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+        for(int i=1;i<m;i++){
+            grid[i][0]+=grid[i-1][0];
+        }
+        for(int j=1;j<n;j++){
+            grid[0][j]+=grid[0][j-1];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                grid[i][j]+=Math.min(grid[i-1][j],grid[i][j-1]);
             }
         }
-        int ans=minSum(grid,m-1,n-1,dp);
-        return ans;
+        return grid[m-1][n-1];
     }
 }
