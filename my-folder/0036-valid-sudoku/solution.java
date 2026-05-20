d72106dfc6f196c9a547ca77ea5a348ac321d640
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-05-20 13:43:02 +0000

    [LeetCode Sync] Runtime - 1 ms (99.97%), Memory - 46.1 MB (91.98%)

--- my-folder/0036-valid-sudoku/solution.java
+++ my-folder/0036-valid-sudoku/solution.java
@@ -1,15 +1,22 @@
-class Solution{
-public boolean isValidSudoku(char[][] board) {
-    Set seen = new HashSet();
-    for (int i=0; i<9; ++i) {
-        for (int j=0; j<9; ++j) {
-            char number = board[i][j];
-            if (number != '.')
-                if (!seen.add(number + " in row " + i) ||
-                    !seen.add(number + " in column " + j) ||
-                    !seen.add(number + " in block " + i/3 + "-" + j/3))
-                    return false;
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean [][]rows=new boolean[9][9];
+        boolean [][]cols=new boolean[9][9];
+        boolean [][]boxes=new boolean[9][9];
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.'){
+                    int num=board[i][j]-'1';
+                    int boxIndex=(i/3)*3+(j/3);
+
+                    if(rows[i][num] || cols[j][num] || boxes[boxIndex][num]){
+                        return false;
+                    }
+                    rows[i][num]=cols[j][num]=boxes[boxIndex][num]=true;
+                }
+            }
         }
+        return true;
     }
-    return true;
-}}
+}
