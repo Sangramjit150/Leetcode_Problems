064f78aa785a0fadb58bc823de637bf7c6b54696
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-05-20 14:34:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.1 MB (23.79%)

--- my-folder/0074-search-a-2d-matrix/solution.java
+++ my-folder/0074-search-a-2d-matrix/solution.java
@@ -1,22 +1,41 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int rows=matrix.length;
-        int columns=matrix[0].length;
-        int left=0;
-        int right=rows*columns-1;
-        while(left<=right){
-            int mid=(left+right)/2;
-            int row=mid/columns;
-            int col=mid%columns;
-            int guess=matrix[row][col];
+    private boolean binarySearch(int[] nums, int target) {
+        // Get the length of the array
+        int n = nums.length;
 
-            if(guess==target)
-            return true;
-            else if(guess<target)
-            left=mid+1;
+        // Initialize low and high pointers
+        int low = 0, high = n - 1;
+
+        // Perform binary search
+        while (low <= high) {
+            // Calculate the middle index
+            int mid = (low + high) / 2;
+
+            // If the middle element is the target, return true
+            if (nums[mid] == target)
+                return true;
+
+            // If target is greater, search in the right half
+            else if (target > nums[mid])
+                low = mid + 1;
+
+            // Otherwise, search in the left half
             else
-            right=mid-1;
+                high = mid - 1;
         }
+
+        // Return false if target is not found
+        return false;
+    }
+    public boolean searchMatrix(int[][] mat, int target) {
+        int n=mat.length;
+        int m=mat[0].length;
+        for(int i=0;i<n;i++){
+            if(mat[i][0]<=target && mat[i][m-1]>=target){
+                return binarySearch(mat[i], target);
+            }
+        }
+       
         return false;
     }
 }
