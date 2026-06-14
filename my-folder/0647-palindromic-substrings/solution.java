155b744f58e3220abbd8e40a41584a295e026337
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-06-14 07:43:40 +0000

    [LeetCode Sync] Runtime - 834 ms (7.36%), Memory - 46.8 MB (14.99%)

---
+++ my-folder/0647-palindromic-substrings/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    private boolean checkPailn(String s){
+        int left=0,right=s.length()-1;
+        while (left<right){
+            if(s.charAt(left)!=s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public int countSubstrings(String s) {
+         int count=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                String sub=s.substring(i,j+1);
+                boolean flag=checkPailn(sub);
+                if(flag)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
