6c60cc14e6f1fac3efa235c1d11ec4073b7b91b3
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-06-29 19:33:22 +0530

    Time: 1 ms (75.75%) | Memory: 43.5 MB (19.94%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,18 +1,8 @@
 class Solution {
     public int numOfStrings(String[] patterns, String word) {
-        HashSet<String>subStrings=new HashSet<>();
-        for(int i=0;i<word.length();i++){
-            for(int j=i;j<word.length();j++){
-                String sub=word.substring(i,j+1);
-                subStrings.add(sub);
-            }
-        }
-        int cnt=0;
-        for(int i=0;i<patterns.length;i++){
-            if(subStrings.contains(patterns[i])){
-                cnt++;
-            }
-        }
-        return cnt;
+        int count = 0;
+        for (String s : patterns)
+            if (word.contains(s)) count++;
+        return count;
     }
 }
